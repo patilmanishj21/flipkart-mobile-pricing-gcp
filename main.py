5f38ef3a07af796e024b61dfcafebd0c9510ecf5
patilmanishj21 <patilmanishj21@gmail.com>
2023-01-09 19:38:45 +0530

    added print pardo

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import logging
 import argparse
 
 
-inputfile="F:/Projects/Flipkart-mobile-prices/flipkart_mobiles.csv"
-outputfile="F:/Projects/Flipkart-mobile-prices/avgprice"
+#inputfile="F:/Projects/Flipkart-mobile-prices/flipkart_mobiles.csv"
+#outputfile="F:/Projects/Flipkart-mobile-prices/avgprice"
 
 class PrintFn(beam.DoFn):
     def process(self, element):
@@ -26,17 +26,18 @@ def replace_space_with_zero(line):
 
 def run(argv=None, save_main_session=True):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inputfile',dest='input', help='Input file to process.')
+    parser.add_argument('--inputfile',dest='input', help='Input file to process.' ,required=True)
     parser.add_argument('--outputfile',dest='output', help='Input file to process.')
 
     known_args, pipline_args = parser.parse_known_args(argv)
     pipelineoptions = PipelineOptions(pipline_args)
     pipelineoptions.view_as(SetupOptions).save_main_session = save_main_session
 
+
     with beam.Pipeline() as p:
 
         maindata=(
-            p| "Reading file" >> beam.io.ReadFromText(inputfile,skip_header_lines=1)   #read file data into pcollection maindata
+            p| "Reading file" >> beam.io.ReadFromText(known_args.input,skip_header_lines=1)   #read file data into pcollection maindata
             |"Split" >> beam.Map(lambda x:x.split(","))
             )
         Gettotalcount=(
@@ -56,7 +57,7 @@ def run(argv=None, save_main_session=True):
                 greater_than_avg | "get avg Count" >> beam.combiners.Count.Globally() | 'print-avg' >> beam.ParDo(PrintFn()) #get avg value count
         )
         Write=(
-                greater_than_avg | "write to file " >> beam.io.WriteToText(outputfile) #write to file
+                greater_than_avg | "write to file " >> beam.io.WriteToText(known_args.output) #write to file
         )
 
 if __name__ == '__main__':
