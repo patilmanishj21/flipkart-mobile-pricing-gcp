4214995a5131e0393b9afa07763ee8e38935da10
patilmanishj21 <patilmanishj21@gmail.com>
2023-01-10 19:41:55 +0530

    added dict for records

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ from apache_beam.options.pipeline_options import SetupOptions
 from statistics import  mean
 import logging
 import argparse
-
+import re
 
 #inputfile="F:/Projects/Flipkart-mobile-prices/flipkart_mobiles.csv"
 #outputfile="F:/Projects/Flipkart-mobile-prices/avgprice"
 
-# class PrintFn(beam.DoFn):
-#     def process(self, element):
-#         print(element)
 
 table_spec = 'flipkart.mobile_prices'
 table_schema='Brand:STRING, Model:STRING, Color:STRING, Memory:STRING, Storage:STRING, Rating:FLOAT, Selling_Price:INT64, Original_Price:INT64'
+
+class DataIngestion:
+    def parse_method(self, string_input):
+        values = re.split(",", re.sub('\r\n', '', re.sub('"', '',string_input)))
+        row = dict(zip(('Brand', 'Model', 'Color', 'Memory', 'Storage', 'Rating','Selling_Price','Original_Price'),values))
+        return row
 
 class GreaterThanAvg(beam.DoFn):
     def process(self, element,side_input):
@@ -35,36 +38,28 @@ def run(argv=None, save_main_session=True):
     pipelineoptions = PipelineOptions(pipline_args)
     pipelineoptions.view_as(SetupOptions).save_main_session = save_main_session
 
+    dataingestion = DataIngestion()
 
     with beam.Pipeline(options=pipelineoptions) as p:
 
         maindata=(
-            p| "Reading file" >> beam.io.ReadFromText(known_args.input,skip_header_lines=1)   #read file data into pcollection maindata
+            p| "Reading file" >> beam.io.ReadFromText(known_args.input,skip_header_lines=1)
             |"Split" >> beam.Map(lambda x:x.split(","))
             )
 
-        # Gettotalcount=(
-        #     maindata | "total Count" >> beam.combiners.Count.Globally() |  'print-total' >> beam.ParDo(PrintFn())  #get total counts of record
-        # )
-
         side_input=(
-         maindata | "Side input pricing " >> beam.Map(lambda x:int(x[7]))  # price column x[7] to calculate avg
+         maindata | "Side input pricing " >> beam.Map(lambda x:int(x[7]))
         )
 
         greater_than_avg=(
-            maindata| "greater than avg" >> beam.ParDo(GreaterThanAvg(),beam.pvalue.AsList(side_input)) #pass the sideinput to Pardo GreaterThanAvg\
-            | "join the data">> beam.Map(lambda x:','.join(x)) \
-            | "replace with zero" >> beam.Map(replace_space_with_zero) \
-            | "Write to BQ" >> beam.io.WriteToBigQuery(table_spec, schema=table_schema,
-                                                       write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
-                                                       create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
+            maindata| "greater than avg" >> beam.ParDo(GreaterThanAvg(),beam.pvalue.AsList(side_input)) \
+            | "join the data">> beam.Map(lambda x:','.join(x))  \
+            | "DataIngetion " >> beam.Map(lambda s: dataingestion.parse_method(s)) \
             #| "write to file " >> beam.io.WriteToText(known_args.output)
-            #| beam.Map(print)
+            | "Write to BQ" >> beam.io.WriteToBigQuery(table_spec, schema=table_schema,
+                                                      write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+                                                       create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
             )
-
-        # Getavgcount = (
-        #         greater_than_avg | "get avg Count" >> beam.combiners.Count.Globally() | 'print-avg' >> beam.ParDo(PrintFn()) #get avg value count
-        # )
 
 
 if __name__ == '__main__':
